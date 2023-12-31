a8dfd354082d94eb07bfdb2bf11622269653b172
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-31 13:53:38 -0300

    Update CircularIntList.java

--- src/main/java/net/vulkanmod/render/chunk/util/CircularIntList.java
+++ src/main/java/net/vulkanmod/render/chunk/util/CircularIntList.java
@@ -3,9 +3,7 @@ package net.vulkanmod.render.chunk.util;
 import org.apache.commons.lang3.Validate;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.Iterator;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Spliterator;
@@ -14,16 +12,21 @@ import java.util.function.Consumer;
 public class CircularIntList {
     private int[] list;
     private final int startIndex;
+
     private int[] previous;
     private int[] next;
 
+    private int currentIndex;
+
     public CircularIntList(int size, int startIndex) {
-        this.startIndex = startcki;
-        generateList(size);
+        this.startIndex = startIndex;
+
+        this.generateList(size);
     }
 
     private void generateList(int size) {
         int[] list = new int[size];
+
         this.previous = new int[size];
         this.next = new int[size];
 
@@ -37,12 +40,6 @@ public class CircularIntList {
             ++k;
         }
 
-        this.previous[0] = -1;
-        System.arraycopy(list, 0, this.previous, 1, size - 1);
-
-        this.next[size - 1] = -1;
-        System.arraycopy(list, 1, this.next, 0, size - 1);
-
         this.list = list;
     }
 
@@ -55,58 +52,57 @@ public class CircularIntList {
     }
 
     public Iterator<Integer> iterator() {
-        return new OwnIterator();
+        return new Iterator<Integer>() {
+            private int currentIndex = -1;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < list.length - 1;
+            }
+
+            @Override
+            public Integer next() {
+                currentIndex++;
+                return list[currentIndex];
+            }
+
+            public int getCurrentIndex() {
+                return currentIndex;
+            }
+
+            public void restart() {
+                currentIndex = -1;
+            }
+        };
     }
 
     public Iterator<Integer> rangeIterator(int startIndex, int endIndex) {
-        return new RangeIterator(startIndex, endIndex);
-    }
+        return new Iterator<Integer>() {
+            private int currentIndex = startIndex - 1;
 
-    public class OwnIterator implements Iterator<Integer> {
-        private final AtomicInteger currentIndex = new AtomicInteger(-1);
-        private final int maxIndex = list.length - 1;
+            @Override
+            public boolean hasNext() {
+                return currentIndex < endIndex - 1;
+            }
 
-        @Override
-        public boolean hasNext() {
-            return currentIndex.get() < maxIndex;
-        }
+            @Override
+            public Integer next() {
+                currentIndex++;
+                try {
+                    return list[currentIndex];
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    throw new RuntimeException();
+                }
+            }
 
-        @Override
-        public Integer next() {
-            currentIndex.incrementAndGet();
-            return list[currentIndex.get()];
-        }
+            public int getCurrentIndex() {
+                return currentIndex;
+            }
 
-        public void restart() {
-            currentIndex.set(-1);
-        }
-    }
-
-    public class RangeIterator implements Iterator<Integer> {
-        private final AtomicInteger currentIndex = new AtomicInteger();
-        private final int startIndex;
-        private final int maxIndex;
-
-        public RangeIterator(int startIndex, int endIndex) {
-            this.startIndex = startIndex;
-            this.maxIndex = endIndex;
-            Validate.isTrue(this.maxIndex < list.length, "Beyond max size");
-            restart();
-        }
-
-        @Override
-        public boolean hasNext() {
-            return currentIndex.get() < maxIndex;
-        }
-
-        @Override
-        public Integer next() {
-            currentIndex.incrementAndGet();
-            return list[currentIndex.get()];
-        }
-
-        public void restart() {
-            currentIndex.set(startIndex - 1);
-        }
+            public void restart() {
+                currentIndex = startIndex - 1;
+            }
+        };
     }
 }
